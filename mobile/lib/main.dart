ed5d04dbfcf925084ab4f16119e7b1b6eb8e8887
Asem287 <asyama.baibarakova@alatoo.edu.kg>
2026-05-21 19:37:55 +0600

    Initialize flutter project structure .

---
+++ mobile/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'screens/login_screen.dart';
+import 'providers/auth_provider.dart';
+import 'providers/map_provider.dart';
+import 'screens/map_screen.dart';
+import 'utils/theme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ChangeNotifierProvider(create: (_) => MapProvider()),
+      ],
+      child: MaterialApp(
+        title: 'Bishkek Courier Vision',
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.darkTheme,
+        home: const MapScreen(),
+      ),
+    );
+  }
+}
